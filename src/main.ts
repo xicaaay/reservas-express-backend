2426654c4c89eec3e53d3437882fde5d4424accf
hxicaaay <xic444y@gmail.com>
2026-01-11 10:06:07 -0600

    ajustar configuración de CORS por entorno

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import 'dotenv/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // CORS
-  app.enableCors();
+  app.enableCors({
+    origin: process.env.CORS_ORIGIN?.split(','),
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type'],
+    credentials: true,
+  });
+
 
   // Swagger
   const config = new DocumentBuilder()
